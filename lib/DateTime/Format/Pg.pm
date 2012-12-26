620d857b30ae12cb84801d90214a094d5572fc87
Daisuke Maki <lestrrat+github@gmail.com>
2012-12-26 15:31:34 +0900

    version++ this be 0.16008

--- lib/DateTime/Format/Pg.pm
+++ lib/DateTime/Format/Pg.pm
@@ -11,7 +11,7 @@ use DateTime::TimeZone 0.06;
 use DateTime::TimeZone::UTC;
 use DateTime::TimeZone::Floating;
 
-$VERSION = '0.16006';
+$VERSION = '0.16008';
 $VERSION = eval $VERSION;
 
 our @ISA = ('DateTime::Format::Builder');
