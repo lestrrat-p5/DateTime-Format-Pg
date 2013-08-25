8b5da515aac05ca28071d1752cc3b2f1a397646f
Daisuke Maki <lestrrat+github@gmail.com>
2013-08-25 13:51:23 +0900

    Checking in changes prior to tagging of version 0.16009.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index ec308f5..1cf0c37 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,8 @@
     Revision history for Perl extension DateTime::Format::Pg.
    
     {{$NEXT}}
    +
    +0.16009 2013-08-25T04:51:15Z
         - Fix typos [pr #2] (dsteinbrunner)
         - Migrate to using Minilla for packaging

--- lib/DateTime/Format/Pg.pm
+++ lib/DateTime/Format/Pg.pm
@@ -11,7 +11,7 @@ use DateTime::TimeZone 0.06;
 use DateTime::TimeZone::UTC;
 use DateTime::TimeZone::Floating;
 
-$VERSION = '0.16008';
+$VERSION = '0.16009';
 $VERSION = eval $VERSION;
 
 our @ISA = ('DateTime::Format::Builder');
