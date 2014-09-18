a17053dba9b09b012491157f8cf9a7fa49995354
Daisuke Maki <lestrrat+github@gmail.com>
2014-09-18 21:36:11 +0900

    Checking in changes prior to tagging of version 0.16010.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 1cf0c37..41cc962 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension DateTime::Format::Pg.
    
     {{$NEXT}}
    
    +0.16010 2014-09-18T12:36:03Z
    +    - Add support for various more interval units [pr #3]:
    +      millenium, century, decade, month, week
    +
     0.16009 2013-08-25T04:51:15Z
         - Fix typos [pr #2] (dsteinbrunner)
         - Migrate to using Minilla for packaging

--- lib/DateTime/Format/Pg.pm
+++ lib/DateTime/Format/Pg.pm
@@ -11,7 +11,7 @@ use DateTime::TimeZone 0.06;
 use DateTime::TimeZone::UTC;
 use DateTime::TimeZone::Floating;
 
-$VERSION = '0.16009';
+$VERSION = '0.16010';
 $VERSION = eval $VERSION;
 
 our @ISA = ('DateTime::Format::Builder');
