547c98b591ab47923ee6877911361cd270be359f
Ilya Chesnokov <chesnokov.ilya@gmail.com>
2017-05-04 14:14:12 +0300

    Round parsed nanosecond to the closest integer value

--- lib/DateTime/Format/Pg.pm
+++ lib/DateTime/Format/Pg.pm
@@ -10,6 +10,7 @@ use DateTime::Format::Builder 0.72;
 use DateTime::TimeZone 0.06;
 use DateTime::TimeZone::UTC;
 use DateTime::TimeZone::Floating;
+use POSIX qw(lround);
 
 $VERSION = '0.16012';
 $VERSION = eval $VERSION;
@@ -368,7 +369,7 @@ sub _fix_timezone {
 sub _fix_nanosecond {
   my %args = @_;
   if(defined $args{'parsed'}->{'nanosecond'}) {
-    $args{'parsed'}->{'nanosecond'} *= 1.0E9;
+    $args{'parsed'}->{'nanosecond'} = lround($args{'parsed'}->{'nanosecond'} * 1.0E9);
   } else {
     delete $args{'parsed'}->{'nanosecond'}
   };
